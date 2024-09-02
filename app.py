58709bbcd5a1e05f26f1da7aee0ffa99ef5e8e69
francobustos23 <bustosfranco071@gmail.com>
2024-09-02 16:05:36 -0300

    TP Keras finish

--- app.py
+++ app.py
@@ -11,6 +11,15 @@ datos = pd.read_csv('altura_peso.csv', sep=',')
 x = datos['Altura'].values
 y = datos['Peso'].values
 
+x = np.nan_to_num(x, nan=np.nanmean(x), posinf=np.nanmean(x), neginf=np.nanmean(x))
+y = np.nan_to_num(y, nan=np.nanmean(y), posinf=np.nanmean(y), neginf=np.nanmean(y))
+
+x_mean, x_std = np.mean(x), np.std(x)
+y_mean, y_std = np.mean(y), np.std(y)
+
+x = (x - x_mean) / x_std
+y = (y - y_mean) / y_std
+
 np.random.seed(2)
 modelo = Sequential()
 modelo.add(Dense(1, input_dim=1, activation='linear')) 
@@ -20,7 +29,7 @@ modelo.compile(loss='mean_squared_error', optimizer=sgd)
 
 modelo.summary()
 
-num_epochs = 100
+num_epochs = 1000
 batch_size = len(x)  
 historial = modelo.fit(x, y, epochs=num_epochs, batch_size=batch_size, verbose=1)
 
